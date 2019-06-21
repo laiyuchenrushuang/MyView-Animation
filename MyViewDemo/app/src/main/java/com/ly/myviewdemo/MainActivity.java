9ea9a95fbf5075ffb80074e66c828ce719123457
ly <383209920@qq.com>
2019-06-21 16:37:41 +0800

    提交按钮基本的UI效果

---
+++ MyViewDemo/app/src/main/java/com/ly/myviewdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ly.myviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *  一直都觉得是难点 ，难点还是必须突破
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
